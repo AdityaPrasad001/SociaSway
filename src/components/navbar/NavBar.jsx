4bd03ded94adf23cf8a44c680924134fe368c004
AdityaPrasad001 <108456699+AdityaPrasad001@users.noreply.github.com>
2023-09-23 21:57:17 +0530

    Navbar
    added navbar in home page

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,8 +1,47 @@
 import React from "react";
 import "./navbar.scss";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="navbar">
+      {/* left */}
+      <div className="left">
+        <Link to="/">
+          <span>SociaSway</span>
+        </Link>
+        <HomeOutlinedIcon />
+        <GridViewOutlinedIcon />
+        <DarkModeOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+
+      {/* right */}
+      <div className="right">
+        <PersonOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="user profile pic"
+          />
+          <span>Alex Johnson</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
